b0aad0b6b7cafc5fab696f05a7b63f98a0112e71
Michael Keenan <michael.keenan@willmottdixon.co.uk>
2016-11-15 17:13:58 +0000

    To clarify...

--- Basket/BasketService.cs
+++ Basket/BasketService.cs
@@ -7,6 +7,8 @@ using Basket.Services.Services.Interfaces;
 
 namespace Basket.Services
 {
+    //THIS IS IN PLACE OF WHERE I WOULD USUALLY USE A CONTROLLER, I JUST DON'T HAVE THE TIME TO SET UP A FULL MVC PROJECT
+    //THAT'S WHY THERE IS NO INTERFACE, BUT I DID DO IT TDD
     public class BasketService
     {
         private ICalculatorService _calculatorService;
