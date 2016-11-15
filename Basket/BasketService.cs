5c9489c57e69095fc701a39deb38b0798de3026a
Michael Keenan <michael.keenan@willmottdixon.co.uk>
2016-11-15 17:04:51 +0000

    Basic Implentation
    Created Projects, Interfaces and initial Tests.
    
    May change

---
+++ Basket/BasketService.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Basket.Services.Models;
+using Basket.Services.Services.Interfaces;
+
+namespace Basket.Services
+{
+    public class BasketService
+    {
+        private ICalculatorService _calculatorService;
+
+        public BasketService(ICalculatorService calculatorService)
+        {
+            _calculatorService = calculatorService;
+        }
+
+        public decimal GetTotal(BasketModel basket)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
